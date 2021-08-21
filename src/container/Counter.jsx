af51609f790ac81e4d1e095723f395eb01089846
tfkhdyt <ta142fa@gmail.com>
2021-08-21 12:24:04 +0700

    hapus swal

--- src/container/Counter.jsx
+++ src/container/Counter.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Cookies from 'universal-cookie';
 import { Howl } from 'howler';
-import Swal from 'sweetalert2';
 
 import suara from '../audio/kaget.wav';
 
