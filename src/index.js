a847fcf979d4c55ce86e8d8977454d1e0d135e8b
tfkhdyt <ta142fa@gmail.com>
2021-08-22 07:13:10 +0700

    nambah component goodbye, tapi disable

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+// import Goodbye from './component/Goodbye';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './css/index.css';
+// import './css/Goodbye.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/*<Goodbye />*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
