6d20fdacff96a0918c44b791546947205f8bc6ee
tfkhdyt <ta142fa@gmail.com>
2021-08-26 21:47:49 +0700

    hapus file config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = config = {
-  API_GET : 'https://leaderboard-popowi.herokuapp.com/api/v1/leaderboards/',
-  API_GET_MAX : 'https://leaderboard-popowi.herokuapp.com/api/v1/leaderboards/max',
-  l
-}
