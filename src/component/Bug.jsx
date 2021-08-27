10aac5799ef45a5fb16599bfe9d9ead1c202f61b
Athallah Dzaki <discordgg5@gmail.com>
2021-08-27 19:20:30 +0700

    Fix Auto Close Github Report bug

--- src/component/Bug.jsx
+++ src/component/Bug.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Bug = () => {
   return (
-    <a className='btn btn-secondary unevent mt-1 mb-0 btn-sm mx-1' href='https://github.com/tfkhdyt/popclick/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D+' target='_blank' rel="noreferrer"><i class="fas fa-bug unevent"></i> Report Bug</a>
+    <a className='btn btn-secondary unevent mt-1 mb-0 btn-sm mx-1' href='https://github.com/tfkhdyt/popclick/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBUG%5D+' rel="noreferrer"><i class="fas fa-bug unevent"></i> Report Bug</a>
   );
 }
 
